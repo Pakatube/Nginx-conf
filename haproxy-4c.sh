4d4af5aa47bcd890faf4a12824b2218f3c67dffd
Pakatube <pakatube@outlook.com>
2024-10-03 12:47:20 +0800

    Create haproxy-4c.sh

---
+++ haproxy-4c.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+apt update
+apt install -y haproxy nginx
+wget https://raw.githubusercontent.com/Pakatube/Nginx-conf/main/haproxy-4c.cfg -O /etc/haproxy/haproxy.cfg
+systemctl restart haproxy
+cat > /etc/rc.local << EOF
+#!/bin/bash
+iptables -F -t nat
+ip6tables -F -t nat
+iptables -t nat -A PREROUTING -p tcp --dport 40000:50000 -j REDIRECT --to-port 65001
+ip6tables -t nat -A PREROUTING -p tcp --dport 40000:50000 -j REDIRECT --to-port 65001
+exit 0
+EOF
+chmod +x /etc/rc.local && /etc/rc.local
+apt-get install cron -y
+(crontab -l 2>/dev/null; echo "* * * * * wget -O /etc/haproxy/haproxy.cfg https://raw.githubusercontent.com/Pakatube/Nginx-conf/main/haproxy-4c.cfg") | crontab -
+(crontab -l 2>/dev/null; echo "0 * * * * /usr/sbin/service haproxy reload") | crontab -
