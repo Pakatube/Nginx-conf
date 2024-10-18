a81aa25c2cca8c2153e2acfe08cbf617d001700a
Pakatube <pakatube@outlook.com>
2024-10-19 01:28:25 +0800

    Update haproxy.sh

--- haproxy.sh
+++ haproxy.sh
@@ -1,17 +1,7 @@
 #!/bin/sh
 apt update
-apt install -y haproxy nginx screen
+apt install -y haproxy nginx screen cron
 wget https://raw.githubusercontent.com/Pakatube/Nginx-conf/main/haproxy.cfg -O /etc/haproxy/haproxy.cfg
 systemctl restart haproxy
-cat > /etc/rc.local << EOF
-#!/bin/bash
-iptables -F -t nat
-ip6tables -F -t nat
-iptables -t nat -A PREROUTING -p tcp --dport 40000:50000 -j REDIRECT --to-port 65001
-ip6tables -t nat -A PREROUTING -p tcp --dport 40000:50000 -j REDIRECT --to-port 65001
-exit 0
-EOF
-chmod +x /etc/rc.local && /etc/rc.local
-apt-get install cron -y
-(crontab -l 2>/dev/null; echo "* * * * * wget -O /etc/haproxy/haproxy.cfg https://raw.githubusercontent.com/Pakatube/Nginx-conf/main/haproxy.cfg") | crontab -
+(crontab -l 2>/dev/null; echo "*/5 * * * * wget -O /etc/haproxy/haproxy.cfg https://raw.githubusercontent.com/Pakatube/Nginx-conf/main/haproxy.cfg") | crontab -
 (crontab -l 2>/dev/null; echo "0 * * * * /usr/sbin/service haproxy reload") | crontab -
