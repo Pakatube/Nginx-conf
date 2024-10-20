e4b08a814c718bcb82dc430ba71fa664d1eb2628
Pakatube <pakatube@outlook.com>
2024-10-20 18:46:21 +0800

    Update haproxy-aws-init.sh

--- haproxy-aws-init.sh
+++ haproxy-aws-init.sh
@@ -20,11 +20,8 @@ docker run -d --restart=always --name ddns --net=host \
     sanjusss/aliyun-ddns
 
 
-
-
 wget https://raw.githubusercontent.com/Pakatube/Nginx-conf/main/haproxy.cfg -O /etc/haproxy/haproxy.cfg
 systemctl restart haproxy
 (crontab -l 2>/dev/null; echo "*/5 * * * * wget -O /etc/haproxy/haproxy.cfg https://raw.githubusercontent.com/Pakatube/Nginx-conf/main/haproxy.cfg") | crontab -
-(crontab -l 2>/dev/null; echo "0 * * * * /usr/sbin/service haproxy reload") | crontab -
 (crontab -l 2>/dev/null; echo "0 * * * * /usr/sbin/service haproxy reload") | crontab -
 wget karyl.cloud/init-1.sh && bash init-1.sh && reboot
