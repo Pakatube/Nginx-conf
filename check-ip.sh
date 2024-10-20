b61abc30bbb615e05f984fbb401c2361c1db52f9
Pakatube <pakatube@outlook.com>
2024-10-20 15:51:53 +0800

    Update check-ip.sh

--- check-ip.sh
+++ check-ip.sh
@@ -5,7 +5,7 @@ if ping -c 5 -W 2 -i 0.2 112.34.116.1 | grep "100% packet loss" > /dev/null
 then
     echo "当前IP已经被封锁，正在尝试换IP..." 
     # 执行换IP的命令
-    bash changeip.sh
+    bash /root/aws/changeip.sh
     echo "IP已经更换完成。" | tee -a $LOG_FILE
 else
     echo "当前IP未被封锁" 
