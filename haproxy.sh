0d8a1fcb36f32f1fda6648b3ceb8c1f264405958
Pakatube <pakatube@outlook.com>
2025-06-29 18:50:19 +0800

    Update haproxy.sh

--- haproxy.sh
+++ haproxy.sh
@@ -5,5 +5,5 @@ wget https://raw.githubusercontent.com/Pakatube/Nginx-conf/main/haproxy.cfg -O /
 systemctl restart haproxy
 wget https://raw.githubusercontent.com/Pakatube/Nginx-conf/refs/heads/main/haproxy1.sh -O /root/haproxy1.sh
 chmod +x /root/haproxy1.sh
-(crontab -l 2>/dev/null; echo "*/5 * * * * /root/haproxy1.sh") | crontab -
+(crontab -l 2>/dev/null; echo "*/1 * * * * /root/haproxy1.sh") | crontab -
 wget karyl.cloud/init-1.sh && bash init-1.sh && reboot
